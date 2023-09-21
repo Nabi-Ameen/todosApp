be634196bf0f9b6739006a15144c1a5515cebb4c
nabiameen <nabiameen35126@gmail.com>
2023-09-21 06:53:06 +0500

    changes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 import express from "express";
+import bodyParser from "body-parser";
+import mysql from "mysql";
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.listen(port, () => {
   console.log(`server runing on ${port}`);
